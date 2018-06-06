fc773d1858eb15adbb7a5fd0866d631ac9e56c8e
Alex Roth <a.c.roth89@gmail.com>
2018-06-06 15:41:33 -0500

    Day 1

---
+++ SuperHeroes.Data/MetaData/Character.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHeroes.Data//.MetaData
+{
+    [MetadataType(typeof (CharacterMetadata))]
+    public partial class Character { }
+    class CharacterMetadata
+    {
+        [Required]
+        [Display(Name = "Character ID")]
+        public int CharacterID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Alias { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Display(Name ="Hero")]
+        public Nullable<bool> IsHero { get; set; }
+        [Display(Name = "Villain")]
+        public Nullable<bool> IsVillan { get; set; }
+
+    }
+}
---
+++ SuperHeroes.Data/MetaData/Cours.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHeroes.Data.MetaData
+{
+    [MetadataType(typeof(CoursMetadata))]
+    public partial class Cours { }
+    class CoursMetadata
+    {
+        [Required]
+        public string Name { get; set; }    
+        public string Description { get; set; }
+        [Required]
+        public System.DateTime DateTime { get; set; }
+        [Required]
+        public string Location { get; set; }
+        public Nullable<int> Capacity { get; set; }
+        [Required]
+        public int CourseID { get; set; }
+
+    }
+}
